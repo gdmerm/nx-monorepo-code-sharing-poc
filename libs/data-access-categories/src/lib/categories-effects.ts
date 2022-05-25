7260b5f845a671e61b6ca90031d5c261a315d5d2
George Mermigkas <george.mermigkas@betssongroup.com>
2022-05-25 16:22:13 +0300

    removes the need for polyfill

--- libs/data-access-categories/src/lib/categories-effects.ts
+++ libs/data-access-categories/src/lib/categories-effects.ts
@@ -6,17 +6,17 @@ import { Axios } from 'axios-observable';
 import { AxiosResponse } from 'axios';
 
 const CATEGORIES_API_LOCATOR = '/api/sb/v1/widgets/categories/v1?includeBadge=false';
-const headers = new Map();
-headers
-  .set('brandid', '4bf6590d-0a29-47f5-a705-42b7a04b7878')
-  .set('x-obg-channel', 'Web')
-  .set('x-obg-device', 'Desktop')
-  .set('marketcode', 'en')
-  .set('x-obg-country-code', 'GR');
+const headers = {
+  'brandid': '',
+  'x-obg-channel': 'Web',
+  'x-obg-device': 'Desktop',
+  'marketcode': 'en',
+  'x-obg-country-code': 'GR'
+}
 
 export const loadCategoriesEpic = (action$: Observable<Action>) => action$.pipe(
   ofType(fetchCategoriesStart),
-  switchMap(() => Axios.get(`http://localhost:3001${CATEGORIES_API_LOCATOR}`, { headers: Object.fromEntries(headers) }).pipe(
+  switchMap(() => Axios.get(`http://localhost:3001${CATEGORIES_API_LOCATOR}`, { headers }).pipe(
     mergeMap((res: AxiosResponse<any>) => ([fetchCategoriesDone(res.data.data.items)])),
     catchError(() => of({ type: '@categories/fetch/http-error' }))
   ))
