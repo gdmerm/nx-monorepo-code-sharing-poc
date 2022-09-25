d4472b7290763e6abe350741952734d8e21415bd
George Mermigkas <george.mermigkas@betssongroup.com>
2022-09-25 16:49:52 +0300

    removes import

--- libs/data-access-giphy/src/lib/giphy-effects.ts
+++ libs/data-access-giphy/src/lib/giphy-effects.ts
@@ -8,8 +8,6 @@ import { AxiosResponse } from 'axios';
 const API_URL = 'https://api.giphy.com/v1/gifs'
 const API_KEY = '8Bgk5JnkH20K6BJE4wvH2JwbsrBJL0Pv'
 
-const SEARCH_API_URL = `${API_URL}/search?api_key=${API_KEY}&q=dogs&limit=25&offset=0&rating=g&lang=en`
-
 export const searchGiphyEffect = (actions$: Observable<Action>) => {
   return actions$.pipe(
     ofType(searchGiphiesStart),
