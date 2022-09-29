1d38e598890825ddc9b6f64352c9cf2d771cabd4
George Mermigkas <george.mermigkas@betssongroup.com>
2022-09-29 10:58:32 +0200

    adds local state to react-native search

--- apps/sb-ios-swift/src/app/giphy-search/GiphySearch.tsx
+++ apps/sb-ios-swift/src/app/giphy-search/GiphySearch.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { GiphyItem, searchGiphiesStart, selectGiphies } from '@betsson-sportsbook-monorepo/data-access-giphy';
 import { TextInput, View, StyleSheet, Image } from 'react-native';
@@ -6,23 +6,29 @@ import { TextInput, View, StyleSheet, Image } from 'react-native';
 
 export function GiphySearch() {
   const dispatch = useDispatch()
+  const [isEmpty, setEmpty] = useState(true)
   const giphies = useSelector(selectGiphies)
-  function handleInput(text: string) {
-    dispatch(searchGiphiesStart(text))
-  }
 
   return (
     <View style={{flex: 1}}>
       <TextInput style={styles.input} onChangeText={handleInput} />
       <View style={styles.container}>
-      {giphies.map((gify: GiphyItem) => (
-        <View key={gify.id}>
-          <Image style={styles.image} source={{uri: gify.images.fixed_height_small.url}} />
-        </View>
-      ))}
+        {!isEmpty && giphies.map((gify: GiphyItem) => (
+          <View key={gify.id}>
+            <Image style={styles.image} source={{uri: gify.images.fixed_height_small.url}} />
+          </View>
+        ))}
       </View>
     </View>
   );
+
+  function handleInput(text: string) {
+    if (text.length === 0) {
+      return setEmpty(true)
+    }
+    setEmpty(false)
+    dispatch(searchGiphiesStart(text))
+  }
 }
 
 const styles = StyleSheet.create({
