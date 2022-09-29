1de63610f5347d484ae9d705e528b52364b78314
George Mermigkas <george.mermigkas@betssongroup.com>
2022-09-29 17:30:12 +0200

    adds distinctKeyUntilChanged

--- libs/data-access-giphy/src/lib/giphy-effects.ts
+++ libs/data-access-giphy/src/lib/giphy-effects.ts
@@ -1,4 +1,12 @@
-import { debounceTime, distinctUntilChanged, filter, mergeMap, Observable, switchMap } from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  distinctUntilKeyChanged,
+  filter,
+  mergeMap,
+  Observable,
+  switchMap
+} from 'rxjs';
 import { Action, AnyAction } from 'redux';
 import { ofType } from 'redux-observable';
 import { searchGiphiesDone, searchGiphiesStart } from './giphy-slice';
@@ -13,7 +21,7 @@ export const searchGiphyEffect = (actions$: Observable<Action>) => {
   return actions$.pipe(
     ofType(searchGiphiesStart),
     filter((action: any) => action.payload.length > 2),
-    distinctUntilChanged(),
+    distinctUntilKeyChanged('payload'),
     debounceTime(150),
     switchMap((action) => Axios.get(`${API_URL}/search?api_key=${API_KEY}&q=${action.payload}&limit=${LIMIT}&offset=0&rating=g&lang=en`).pipe(
       mergeMap((res: AxiosResponse) => ([searchGiphiesDone(res.data.data)]))
